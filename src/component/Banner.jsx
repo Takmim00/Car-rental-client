45c90d6e220ddfa8cff3e2eb259010f86a9f94c6
Takmim00 <takmimm@gmail.com>
2025-02-11 09:53:33 +0600

    banner updated

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -4,7 +4,7 @@ import { FaArrowRight } from "react-icons/fa6";
 const Banner = () => {
   return (
     <div
-      className="relative w-full  bg-cover bg-center lg:h-[80vh] flex md:flex-row flex-col items-center justify-between px-10 "
+      className="relative w-full  bg-cover bg-center lg:h-[70vh] flex md:flex-row flex-col items-center justify-between px-10 "
       style={{
         backgroundImage: `url('https://i.ibb.co.com/T1CXCH4/bg-car.jpg')`,
       }}
@@ -13,12 +13,12 @@ const Banner = () => {
         <h1 className="text-4xl md:text-6xl font-bold  leading-tight">
           Drive Your Dreams Today!
         </h1>
-        <p className="text-lg text-gray-500 mt-4">
+        <p className="text-lg text-gray-600 mt-4">
           Fulfill your automotive fantasies without breaking the bank. Check our
           affordable car rentals for an opulent yet economical ride.
         </p>
         <Link to="/availableCar">
-          <button className="mt-6 btn bg-red-400 hover:bg-red-500 text-white rounded-full   text-lg font-semibold shadow-md text-center">
+          <button className="mt-6 btn bg-red-400 hover:bg-red-500 text-white rounded-full flex justify-center  text-lg font-semibold shadow-md text-center">
             View Available Cars <FaArrowRight />
           </button>
         </Link>
